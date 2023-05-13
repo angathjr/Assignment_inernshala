d7c7211c825fa0e46d7adb1889e740386f5d18f6
angathjr <angathjr@gmail.com>
2023-05-13 15:14:37 +0530

    minor changes

--- lib/controllers/destination_controller.dart
+++ lib/controllers/destination_controller.dart
@@ -1,16 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-
 import '../models/destination.dart';
 
 class DestinationController extends GetxController {
   var lakes = DestinationModel().obs;
   var isLoading = true.obs;
+  var currentIndex = 0.obs;
+  var isScrolling = false.obs;
+
+  final ScrollController scrollController = ScrollController();
 
   @override
   void onInit() {
     super.onInit();
     loadData();
+    scrollController.addListener(() {
+      handleScroll();
+    });
+  }
+
+  @override
+  void onClose() {
+    scrollController.removeListener(handleScroll);
+    super.onClose();
   }
 
   void loadData() async {
@@ -18,5 +31,18 @@ class DestinationController extends GetxController {
     final jsonString = await rootBundle.loadString('assets/data.json');
     lakes.value = destinationModelFromJson(jsonString);
     isLoading(false);
+  }
+
+  void changeIndex(int index) {
+    currentIndex.value = index;
+  }
+
+  void handleScroll() {
+    if (scrollController.offset != scrollController.position.maxScrollExtent &&
+        scrollController.offset != 0) {
+      isScrolling.value = true;
+    } else {
+      isScrolling.value = false;
+    }
   }
 }
--- lib/views/destination_screen.dart
+++ lib/views/destination_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 import 'package:internshala_assignment/controllers/destination_controller.dart';
 import 'package:internshala_assignment/views/widgets/popular_trek_widget.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class DestinationScreen extends StatelessWidget {
   DestinationScreen({super.key});
@@ -16,170 +17,199 @@ class DestinationScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-          child: Obx(
-        () => (controller.isLoading.value == true)
-            ? const Center(child: CircularProgressIndicator())
-            : CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          alignment: Alignment.center,
-                          width: width * 0.09,
-                          height: width * 0.09,
-                          decoration: const BoxDecoration(
-                              shape: BoxShape.circle, color: Colors.white),
-                          child: Icon(
-                            Icons.arrow_back_ios,
-                            color: Colors.black,
-                            size: width * 0.04,
+        child: Obx(
+          () => (controller.isLoading.value == true)
+              ? const Center(child: CircularProgressIndicator())
+              : CustomScrollView(
+                  controller: controller.scrollController,
+                  slivers: [
+                    SliverAppBar(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            width: width * 0.09,
+                            height: width * 0.09,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              color: Colors.black,
+                              size: width * 0.04,
+                            ),
                           ),
+                          Container(
+                            alignment: Alignment.center,
+                            width: width * 0.09,
+                            height: width * 0.09,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                            child: Icon(
+                              Icons.search,
+                              fill: 1,
+                              color: Colors.black,
+                              size: width * 0.05,
+                            ),
+                          )
+                        ],
+                      ),
+                      expandedHeight: height * 0.35,
+                      flexibleSpace: FlexibleSpaceBar(
+                        background:
+
+                            //the slider comes here as carousel slider
+
+                            Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            SizedBox(
+                              width: width,
+                              child: CarouselSlider.builder(
+                                  itemCount: controller
+                                      .lakes.value.bannerImages!.length,
+                                  itemBuilder: (context, index, realIndex) {
+                                    return ClipRRect(
+                                      borderRadius: const BorderRadius.only(
+                                          bottomLeft: Radius.circular(25),
+                                          bottomRight: Radius.circular(25)),
+                                      child: Image.network(
+                                        controller
+                                            .lakes.value.bannerImages![index],
+                                        fit: BoxFit.cover,
+                                      ),
+                                    );
+                                  },
+                                  options: CarouselOptions(
+                                      autoPlay: controller.isScrolling.value
+                                          ? false
+                                          : true,
+                                      disableCenter: true,
+                                      viewportFraction: 1,
+                                      onPageChanged: ((index, reason) =>
+                                          controller.changeIndex(index)))),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(bottom: height * 0.02),
+                              alignment: Alignment.bottomCenter,
+                              child: AnimatedSmoothIndicator(
+                                effect: const WormEffect(
+                                    dotHeight: 10,
+                                    dotWidth: 13,
+                                    dotColor: Colors.grey,
+                                    activeDotColor: Colors.white),
+                                activeIndex: controller.currentIndex.value,
+                                count:
+                                    controller.lakes.value.bannerImages!.length,
+                              ),
+                            ),
+                          ],
                         ),
-                        Container(
-                          alignment: Alignment.center,
-                          width: width * 0.09,
-                          height: width * 0.09,
-                          decoration: const BoxDecoration(
-                              shape: BoxShape.circle, color: Colors.white),
-                          child: Icon(
-                            Icons.search,
-                            color: Colors.black,
-                            size: width * 0.05,
-                          ),
-                        )
-                      ],
+                      ),
                     ),
-                    expandedHeight: height * 0.35,
-                    flexibleSpace: FlexibleSpaceBar(
-                      background:
+                    SliverPadding(
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.03),
+                      sliver: SliverList(
+                        delegate: SliverChildListDelegate([
+                          //rating section
 
-                          //the slider comes here as carousel slider
-
+                          RatingBar.builder(
+                            initialRating: controller.lakes.value.rating!,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemPadding:
+                                EdgeInsets.symmetric(horizontal: width * 0.004),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star_rate_rounded,
+                              color: Colors.amber,
+                            ),
+                            onRatingUpdate: (rating) {},
+                          ),
                           SizedBox(
-                        width: width,
-                        child: CarouselSlider.builder(
-                            itemCount:
-                                controller.lakes.value.bannerImages!.length,
-                            itemBuilder: (context, index, realIndex) {
-                              return ClipRRect(
-                                borderRadius: const BorderRadius.only(
-                                    bottomLeft: Radius.circular(25),
-                                    bottomRight: Radius.circular(25)),
-                                child: Image.network(
-                                  controller.lakes.value.bannerImages![index],
-                                  fit: BoxFit.cover,
+                            height: height * 0.02,
+                          ),
+                          Text(
+                            "${controller.lakes.value.description}",
+                            style: TextStyle(
+                                fontSize: width * 0.042, color: Colors.grey),
+                          ),
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
+                          ListView.builder(
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: width * 0.05),
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.circle,
+                                      size: width * 0.02,
+                                      color: Colors.grey,
+                                    ),
+                                    SizedBox(
+                                      width: width * 0.09,
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                          controller
+                                              .lakes.value.details![index],
+                                          softWrap: true,
+                                          style: TextStyle(
+                                              fontSize: width * 0.037,
+                                              color: Colors.grey,
+                                              fontWeight: FontWeight.w600)),
+                                    )
+                                  ],
                                 ),
                               );
                             },
-                            options: CarouselOptions(
-                                autoPlay: true,
-                                disableCenter: true,
-                                viewportFraction: 1)),
+                            itemCount: controller.lakes.value.details!.length,
+                          ),
+                          SizedBox(
+                            height: height * 0.02,
+                          ),
+                          Text(
+                            "Popular Treks",
+                            style: TextStyle(
+                                fontSize: width * 0.05,
+                                fontWeight: FontWeight.w600),
+                          ),
+
+                          SizedBox(
+                            height: height * 0.2,
+                            child: ListView.builder(
+                              physics: const BouncingScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              itemBuilder: ((context, index) =>
+                                  PopularTrekWidget(
+                                    width: width,
+                                    height: height,
+                                    controller: controller,
+                                    index: index,
+                                  )),
+                              itemCount:
+                                  controller.lakes.value.popularTreks!.length,
+                            ),
+                          ),
+
+                          //added space for just to scroll and make sure the slider collapses
+                          SizedBox(
+                            height: height * 0.4,
+                          )
+                        ]),
                       ),
                     ),
-                  ),
-                  SliverPadding(
-                    padding: EdgeInsets.symmetric(horizontal: width * 0.03),
-                    sliver: SliverList(
-                      delegate: SliverChildListDelegate([
-                        //rating section
-
-                        RatingBar.builder(
-                          initialRating: controller.lakes.value.rating!,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          itemPadding:
-                              EdgeInsets.symmetric(horizontal: width * 0.004),
-                          itemBuilder: (context, _) => const Icon(
-                            Icons.star_rate_rounded,
-                            color: Colors.amber,
-                          ),
-                          onRatingUpdate: (rating) {},
-                        ),
-                        SizedBox(
-                          height: height * 0.02,
-                        ),
-                        Text(
-                          "${controller.lakes.value.description}",
-                          style: TextStyle(
-                              fontSize: width * 0.042, color: Colors.grey),
-                        ),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        ListView.builder(
-                          physics: const BouncingScrollPhysics(),
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: width * 0.05),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.circle,
-                                    size: width * 0.02,
-                                    color: Colors.grey,
-                                  ),
-                                  SizedBox(
-                                    width: width * 0.09,
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                        controller.lakes.value.details![index],
-                                        softWrap: true,
-                                        style: TextStyle(
-                                            fontSize: width * 0.037,
-                                            color: Colors.grey,
-                                            fontWeight: FontWeight.w600)),
-                                  )
-                                ],
-                              ),
-                            );
-                          },
-                          itemCount: controller.lakes.value.details!.length,
-                        ),
-                        SizedBox(
-                          height: height * 0.02,
-                        ),
-                        Text(
-                          "Popular Treks",
-                          style: TextStyle(
-                              fontSize: width * 0.05,
-                              fontWeight: FontWeight.w600),
-                        ),
-
-                        SizedBox(
-                          height: height * 0.2,
-                          child: ListView.builder(
-                            physics: const BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            shrinkWrap: true,
-                            itemBuilder: ((context, index) => PopularTrekWidget(
-                                  width: width,
-                                  height: height,
-                                  controller: controller,
-                                  index: index,
-                                )),
-                            itemCount:
-                                controller.lakes.value.popularTreks!.length,
-                          ),
-                        ),
-
-                        //added space for just to scroll and make sure the slider collapses
-                        SizedBox(
-                          height: height * 0.4,
-                        )
-                      ]),
-                    ),
-                  ),
-                ],
-              ),
-      )),
+                  ],
+                ),
+        ),
+      ),
     );
   }
 }
